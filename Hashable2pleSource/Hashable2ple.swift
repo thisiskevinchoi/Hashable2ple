43e87afa768f581693b735a6c6b81c380632e657
rudifa <rudi.farkas@gmail.com>
2018-06-29 19:53:55 +0200

    add a test per example in README; a warning remains re -F/ option

--- Hashable2pleSource/Hashable2ple.swift
+++ Hashable2pleSource/Hashable2ple.swift
@@ -11,7 +11,7 @@ import Foundation
 struct Hashable2ple<P1, P2>: Hashable where P1: Hashable, P2: Hashable {
     var firstProperty: P1
     var secondProperty: P2
-    init(firstProperty: P1, secondProperty: P2) {
+    init(_ firstProperty: P1, _ secondProperty: P2) {
         self.firstProperty = firstProperty
         self.secondProperty = secondProperty
     }
