86d4dbaad677b2e7f04e8350343a9c4477d5ba0d
rudifa <rudi.farkas@gmail.com>
2018-06-29 19:19:11 +0200

    build in Xcode 9.4.1

--- Hashable2pleSource/Hashable2ple.swift
+++ Hashable2pleSource/Hashable2ple.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Hashable2ple<P1, P2 where P1: Hashable, P2: Hashable>: Hashable {
+struct Hashable2ple<P1, P2>: Hashable where P1: Hashable, P2: Hashable {
     var firstProperty: P1
     var secondProperty: P2
     init(firstProperty: P1, secondProperty: P2) {
